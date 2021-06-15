fb5445b66a5179ad3a6ce82abc1c45cdac8e7553
ilkerogutcu <ilkerogtc@gmail.com>
2021-06-15 18:23:01 +0300

    feat(dataAccess): Added identity context

---
+++ src/StarterProject.DataAccess/Concrete/EntityFramework/Contexts/IdentityContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using StarterProject.Entities.Concrete;
+
+namespace StarterProject.DataAccess.Concrete.EntityFramework.Contexts
+{
+    public class IdentityContext : IdentityDbContext<ApplicationUser>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-CQ5O8N7;Database=starterProject;Trusted_Connection=True;");
+        }
+    }
+}
