c5579b502aecdc584fbb4eaf3af0137106ab3ca1
ilker <ilkerogtc@gmail.com>
2021-06-15 22:09:02 +0300

    feat(core): Added Elasticsearch logger and Mongo Database logger

---
+++ src/StarterProject.Core/CrossCuttingConcerns/Logging/Serilog/Loggers/MongoDbLogger.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using StarterProject.Core.CrossCuttingConcerns.Logging.Serilog.ConfigurationModels;
+using StarterProject.Core.Utilities.IoC;
+using StarterProject.Core.Utilities.Messages;
+
+namespace StarterProject.Core.CrossCuttingConcerns.Logging.Serilog.Loggers
+{
+    public class MongoDbLogger : LoggerServiceBase
+    {
+        public MongoDbLogger()
+        {
+            var configuration = ServiceTool.ServiceProvider.GetService<IConfiguration>();
+            
+            var logConfig =
+                configuration.GetSection("SeriLogConfigurations:MongoDbConfiguration") as MongoDbConfiguration
+                ?? throw new Exception(SerilogMessages.NullOptionsMessage);
+            
+            Logger = new LoggerConfiguration()
+                .WriteTo.MongoDB(logConfig.ConnectionString)
+                .WriteTo.Seq(logConfig.SeqConnectionString)
+                .CreateLogger();
+        }
+    }
+}
