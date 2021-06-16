691e3320d9e66081e5fd1e0e15765f80bcea71cd
ilker <ilkerogtc@gmail.com>
2021-06-16 22:58:57 +0300

    feat(business): Added autofac business module

---
+++ src/StarterProject.Business/DependencyResolvers/AutofacBusinessModule.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+using System.Reflection;
+using Autofac;
+using Autofac.Extras.DynamicProxy;
+using Castle.DynamicProxy;
+using StarterProject.Core.Utilities.Interceptors;
+using Module = Autofac.Module;
+
+namespace StarterProject.Business.DependencyResolvers
+{
+    public class AutofacBusinessModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            
+            var assembly = Assembly.GetExecutingAssembly();
+
+            builder.RegisterAssemblyTypes(assembly).AsImplementedInterfaces()
+                .EnableInterfaceInterceptors(new ProxyGenerationOptions
+                {
+                    Selector = new AspectInterceptorSelector()
+                }).SingleInstance();
+        }
+    }
+}
