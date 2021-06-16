38590656db851da791f7412f6a3af384aa4ff942
ilker <ilkerogtc@gmail.com>
2021-06-16 23:09:29 +0300

    feat(business): Added get date range function on utilities

---
+++ src/StarterProject.Business/Constants/Utilities.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StarterProject.Business.Constants
+{
+    public static class Utilities
+    {
+        public static IEnumerable<DateTime> GetDateRange(DateTime startDate, DateTime endDate)
+        {
+            if (endDate < startDate)
+                throw new ArgumentException("endDate must be greater than or equal to startDate");
+
+            while (startDate <= endDate)
+            {
+                yield return startDate;
+                startDate = startDate.AddDays(1);
+            }
+        }
+    }
+}
+
