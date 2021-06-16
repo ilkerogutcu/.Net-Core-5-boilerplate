1f3980e26260069e0d8668f914d42729cc8060ad
ilker <ilkerogtc@gmail.com>
2021-06-16 23:08:37 +0300

    feat(business): Added messages

---
+++ src/StarterProject.Business/Constants/Messages.cs
@@ -0,0 +1,20 @@
+﻿namespace StarterProject.Business.Constants
+{
+    public static class Messages
+    {
+        public const string UserAlreadyExist = "User already exist!";
+        public const string SignInSuccessfully = "Sign in successfully.";
+        public const string RequiredTwoFactoryCode = "Required two factory code!";
+        public const string SignInFailed = "Sign in failed!";
+        public const string UserNotFound = "User not found!";
+        public const string FailedToRegisterNewUser = "Failed to register new user!";
+        public const string UserCreatedSuccessfully = "User created successfully.";
+        public const string TokenCreatedSuccessfully = "Token created successfully.";
+        public const string FailedToCreateToken = "Failed to create token!";
+        public const string EmailSuccessfullyConfirmed = "Email successfully confirmed.";
+        public const string ErrorVerifyingMail = "There was an error verifying email!";
+        public const string FailedToUpdateUser = "Failed to update user!";
+        public const string UpdatedUserSuccessfully = "User updated succesfully.";
+        public const string GetDateRangeError = "End date must be greater than or equal to start date!";
+    }
+}
