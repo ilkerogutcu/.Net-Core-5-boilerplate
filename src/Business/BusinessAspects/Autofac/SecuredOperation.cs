69e13e5993bae3eab0d85fac598ea5d40ddafbe0
ilker <ilkerogtc@gmail.com>
2021-07-14 01:37:15 +0300

    feat(business): Secured operation aspect added

---
+++ src/Business/BusinessAspects/Autofac/SecuredOperation.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using Business.Constants;
+using Castle.DynamicProxy;
+using Core.Utilities.Interceptors;
+using Core.Utilities.IoC;
+using Entities.Enums;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Business.BusinessAspects.Autofac
+{
+    /// <summary>
+    /// This Aspect control the user's roles in HttpContext by inject the IHttpContextAccessor.
+    /// It is checked by writing as [SecuredOperation] on the handler.
+    /// If a valid authorization cannot be found in aspect, it throws an exception.
+    /// </summary>
+    public class SecuredOperation : MethodInterception
+    {
+        private readonly Roles[] _roles;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public SecuredOperation(params Roles[] roles)
+        {
+            _roles = roles;
+            _httpContextAccessor = ServiceTool.ServiceProvider.GetService<IHttpContextAccessor>();
+        }
+
+        protected override void OnBefore(IInvocation invocation)
+        {
+            if (_roles.Any(role => _roles.Any(x => _httpContextAccessor.HttpContext.User.IsInRole(role.ToString()))))
+            {
+                return;
+            }
+
+            throw new Exception(Messages.AuthorizationsDenied);
+        }
+    }
+}
--- src/WebAPI/Startup.cs
+++ src/WebAPI/Startup.cs
@@ -32,16 +32,45 @@ namespace WebAPI
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
+        private IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo {Title = "StarterProject.WebAPI", Version = "v1"});
-            });
+            services.AddSwaggerGen(swagger =>  
+            {  
+                //This is to generate the Default UI of Swagger Documentation  
+                swagger.SwaggerDoc("v1", new OpenApiInfo  
+                {   
+                    Version= "v1",   
+                    Title = "Starter Project WEB API",  
+                    Description="Starter Project WEB API" });  
+                // To Enable authorization using Swagger (JWT)  
+                swagger.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()  
+                {  
+                    Name = "Authorization",  
+                    Type = SecuritySchemeType.ApiKey,  
+                    Scheme = "Bearer",  
+                    BearerFormat = "JWT",  
+                    In = ParameterLocation.Header,  
+                    Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\"",  
+                });  
+                swagger.AddSecurityRequirement(new OpenApiSecurityRequirement  
+                {  
+                    {  
+                        new OpenApiSecurityScheme  
+                        {  
+                            Reference = new OpenApiReference  
+                            {  
+                                Type = ReferenceType.SecurityScheme,  
+                                Id = "Bearer"  
+                            }  
+                        },
+                        System.Array.Empty<string>()
+                    }  
+                });  
+            });  
             services.AddDependencyResolvers(new ICoreModule[]
             {
                 new CoreModule(),
@@ -83,7 +112,7 @@ namespace WebAPI
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public static void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             ServiceTool.ServiceProvider = app.ApplicationServices;
 
